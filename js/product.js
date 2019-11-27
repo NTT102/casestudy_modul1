c38bfaffc6e8389e2f542866e404e0bbbcb71359
NTT102 <thanhtungg.2496@gmail.com>
2019-11-28 02:51:57 +0700

    update

--- js/product.js
+++ js/product.js
@@ -1,59 +1,118 @@
+//login
+function check() {
+   let username =  document.getElementById('username').value;
+   let password =  document.getElementById('password').value;
+   if(username == "admin" && password == "123123") {
+       alert("đăng nhập thành công")
+       return window.location = "product.html";
+   } else {
+       alert("sai tên đăng nhập hoặc mật khẩu");
+   }
 
+}
 
-this.addProduct = function () {
-    let product = document.getElementById("newProduct").value;
-    if (product === "") {
-        alert("Vui long nhap vao ten.");
-    } else {
-        this.listProducts.push([product, ,]);
-        this.setLocalStorageListProducts();
-        return this.displayProductsAdmin();
-    }
-};
 
 let products = function () {
     //list product
     this.listProducts = [
-        ["tung", "hoc code","C:\Users\NTT\Desktop","30","200.000"]
+        {
+            name:'Iphone 6',
+            cate:'Apple',
+            image:'https://demo-hinhanh.png',
+            total:30,
+            price:8000000,
+            status:'true',
+        },
+        {
+            name:'samsung galaxy A30s',
+            cate:'Samsung',
+            image:'https://demo-hinhanh.png',
+            total:15,
+            price:6500000,
+            status:'true',
+        },
+        {
+            name:'oppo RENO 2',
+            cate:'Samsung',
+            image:'https://demo-hinhanh.png',
+            total:18,
+            price:14000000,
+            status:'true',
+        },
+        {
+            name:'Pin sạc dự phòng Li-on 10.000mAh',
+            cate:'phụ kiện',
+            image:'https://demo-hinhanh.png',
+            total:15,
+            price:1450000,
+            status:'true',
+        }
     ];
+
+    //saveStore
+    this.saveStore = function(){
+        sessionStorage.setItem('products', JSON.stringify(this.listProducts));
+    }
     //add
     this.addProduct = function () {
-        let danhmuc = document.getElementById('danhmuc').value;
+        let cate = document.getElementById('danhmuc').value;
         let name = document.getElementById('sanpham').value;
-        let anh = document.getElementById('anh').value;
-        let soluong = document.getElementById('soluong').value;
-        let dongia = document.getElementById('dongia').value;
-        let sp = [name, danhmuc, anh, soluong, dongia]
-        if (danhmuc === "" && name === "") {
-            alert("Vui long nhap vao ten.");
+        let image = document.getElementById('anh').value;
+        let total = document.getElementById('soluong').value;
+        let price = document.getElementById('dongia').value;
+        let status =  document.getElementById('status').value;
+        if (cate === "" || name === "" || image === "" || total === "" || price === "" ) {
+            alert("Vui long nhập đủ thông tin.");
         } else {
-        this.listProducts.push(sp);
+        this.listProducts.push({name,cate,image,total,price,status});
+        this.saveStore();
         this.showProduct();
         }
     }
     //edit
-    this.edit = function () {
-        
-
+    this.edit = function (i) {
+        document.getElementById("product-"+i).contentEditable = true;
+        this.saveStore();
     }
     this.delete = function (i) {
         this.listProducts.splice(i,1);
+        this.saveStore();
        this.showProduct();
     }
+
+    this.save = function(i){
+        let value =  document.getElementById("product-"+i).children
+        let arr = [];
+        for(let i = 0; i < value.length;i++){
+            arr.push(value[i].textContent)
+        }
+        let spEdit = {name:arr[1],cate:arr[2],image:arr[3],total:arr[4],price:arr[5],status:arr[6]}
+        this.listProducts[i] = spEdit 
+        this.saveStore();
+        this.showProduct();
+    }
+
     this.showProduct = function () {
         let sout = '';
+        let productStore =  JSON.parse(sessionStorage.getItem("products"));
+        if(productStore){
+            this.listProducts = productStore;
+        }
         for (i = 0; i < this.listProducts.length; i++) {
-            sout += '<ul class ="danhsach-item">';
+            sout += '<ul class ="danhsach-item" id="product-'+i+'">';
             sout += '<li style="width:60px;">' + (i + 1) + '</li>';
-            sout += '<li style="width:250px;">' + this.listProducts[i][0] + '</li>';
-            sout += '<li style="width:200px;">' + this.listProducts[i][1] + '</li>';
-            sout += '<li style="width:200px;">' + this.listProducts[i][2] + '</li>';
-            sout += '<li style="width:200px;">' + this.listProducts[i][3] + '</li>';
-            sout += '<li style="width:130px;">' + this.listProducts[i][4] + '</li>';
-            sout += '<li><button class="button" type="button" onclick="deleteSp('+i+')" style="float: right;">Xoa</button>'
-            sout += '<li><button class="button" type="button" onclick="deleteSp('+i+')" style="float: right;">Sửa</button>'
+            sout += '<li style="width:250px;">' + this.listProducts[i].name + '</li>';
+            sout += '<li style="width:200px;">' + this.listProducts[i].cate + '</li>';
+            sout += '<li style="width:200px;">' + this.listProducts[i].image + '</li>';
+            sout += '<li style="width:200px;">' + this.listProducts[i].total + '</li>';
+            sout += '<li style="width:130px;">' + this.listProducts[i].price + '</li>';
+            sout += '<li style="width:130px;">' + this.listProducts[i].status + '</li>';
+            sout += '<li><button class="button" type="button" onclick="deleteSp('+i+')">Xoa</button>';
+            sout += '<li><button class="button" type="button" onclick="editSp('+i+')">Sửa</button>';
+            sout += '<li><button class="button" type="button" onclick="saveSp('+i+')">Save</button>';
             sout += '</ul>';
         }
+        this.saveStore();
         document.getElementById('result').innerHTML = sout;
     }
 }
@@ -74,6 +133,16 @@ function deleteSp(i){
 
 function add(){
     pro.addProduct();
+    
 }
+
+function editSp(i){
+    pro.edit(i);
+}
+
+function saveSp(i){
+    pro.save(i);
+}
+
 
 
