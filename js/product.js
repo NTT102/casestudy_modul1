23a21b9f1ded32aca860943546b7e2a776fb80a4
tung <thanhtungg.2496@gmail.com>
2019-11-28 09:19:38 +0700

    end

--- js/product.js
+++ js/product.js
@@ -1,13 +1,15 @@
 //login
 function check() {
-   let username =  document.getElementById('username').value;
-   let password =  document.getElementById('password').value;
-   if(username == "admin" && password == "123123") {
-       alert("đăng nhập thành công")
-       return window.location = "product.html";
-   } else {
-       alert("sai tên đăng nhập hoặc mật khẩu");
-   }
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    if (username == "admin" && password == "123123") {
+        alert("đăng nhập thành công")
+        return true;
+        //    return window.location = "product.html";
+    } else {
+        alert("sai tên đăng nhập hoặc mật khẩu");
+        return false;
+    }
 
 }
 
@@ -16,41 +18,41 @@ let products = function () {
     //list product
     this.listProducts = [
         {
-            name:'Iphone 6',
-            cate:'Apple',
-            image:'iphone_6.jpg',
-            total:30,
-            price:'8.000.000',
-            status:'true',
+            name: 'Iphone 6',
+            cate: 'Apple',
+            image: 'iphone_6.jpg',
+            total: 30,
+            price: '8.000.000',
+            status: 'true',
         },
         {
-            name:'samsung galaxy A30s',
-            cate:'Samsung',
-            image:'Samsung_A30s.png',
-            total:15,
-            price:'6.500.000',
-            status:'true',
+            name: 'samsung galaxy A30s',
+            cate: 'Samsung',
+            image: 'Samsung_A30s.png',
+            total: 15,
+            price: '6.500.000',
+            status: 'true',
         },
         {
-            name:'oppo RENO 2',
-            cate:'Samsung',
-            image:'Oppo_reno2.png',
-            total:18,
-            price:'14.000.000',
-            status:'true',
+            name: 'oppo RENO 2',
+            cate: 'Samsung',
+            image: 'Oppo_reno2.png',
+            total: 18,
+            price: '14.000.000',
+            status: 'true',
         },
         {
-            name:'Pin sạc dự phòng Li-on 10.000mAh',
-            cate:'phụ kiện',
-            image:'Pin_lion.jpg',
-            total:15,
-            price:'1.450.000',
-            status:'true',
+            name: 'Pin sạc dự phòng Li-on 10.000mAh',
+            cate: 'phụ kiện',
+            image: 'Pin_lion.jpg',
+            total: 15,
+            price: '1.450.000',
+            status: 'true',
         }
     ];
 
     //saveStore
-    this.saveStore = function(){
+    this.saveStore = function () {
         sessionStorage.setItem('products', JSON.stringify(this.listProducts));
     }
     //add
@@ -60,56 +62,59 @@ let products = function () {
         let image = document.getElementById('anh').files[0].name;
         let total = document.getElementById('soluong').value;
         let price = document.getElementById('dongia').value;
-        let status =  document.getElementById('status').value;
-        if (cate === "" || name === "" || image === ""  || total === "" || price === "" ) {
+        let status = document.getElementById('status').value;
+        if (cate === "" || name === "" || image === "" || total === "" || price === "") {
             alert("Vui long nhập đủ thông tin.");
         } else {
-        this.listProducts.push({name,cate,image,total,price,status});
-        this.saveStore();
-        this.showProduct();
+            this.listProducts.push({ name, cate, image, total, price, status });
+            this.saveStore();
+            this.showProduct();
         }
     }
     //edit
     this.edit = function (i) {
-        document.getElementById("product-"+i).contentEditable = true;
+        document.getElementById("product-" + i).contentEditable = true;
         this.saveStore();
     }
+    //delete
     this.delete = function (i) {
-        this.listProducts.splice(i,1);
+        if ( confirm("bạn chắc muốn xoá chứ?") == true) {
+            this.listProducts.splice(i, 1);
+        }
         this.saveStore();
-       this.showProduct();
+        this.showProduct();
     }
-
-    this.save = function(i){
-        let value =  document.getElementById("product-"+i).children
+    // show
+    this.save = function (i) {
+        let value = document.getElementById("product-" + i).children
         let arr = [];
-        for(let i = 0; i < value.length;i++){
+        for (let i = 0; i < value.length; i++) {
             arr.push(value[i].textContent)
         }
-        let spEdit = {name:arr[1],cate:arr[2],image:arr[3],total:arr[4],price:arr[5],status:arr[6]}
-        this.listProducts[i] = spEdit 
+        let spEdit = { name: arr[1], cate: arr[2], image: arr[3], total: arr[4], price: arr[5], status: arr[6] }
+        this.listProducts[i] = spEdit
         this.saveStore();
         this.showProduct();
     }
 
     this.showProduct = function () {
         let sout = '';
-        let productStore =  JSON.parse(sessionStorage.getItem("products"));
-        if(productStore){
+        let productStore = JSON.parse(sessionStorage.getItem("products"));
+        if (productStore) {
             this.listProducts = productStore;
         }
         for (i = 0; i < this.listProducts.length; i++) {
-            sout += '<ul class ="danhsach-item" id="product-'+i+'">';
+            sout += '<ul class ="danhsach-item" id="product-' + i + '">';
             sout += '<li style="width:60px;">' + (i + 1) + '</li>';
             sout += '<li style="width:250px;">' + this.listProducts[i].name + '</li>';
             sout += '<li style="width:200px;">' + this.listProducts[i].cate + '</li>';
-            sout += '<li style="width:200px;">' + '<a target="_blank" href="E:/casestudy_modul1/img/'+this.listProducts[i].image +'" > '+ this.listProducts[i].image +' </a>' + '</li>';
-            sout += '<li style="width:200px;">' + this.listProducts[i].total  + '</li>';
+            sout += '<li style="width:200px;">' + '<a target="_blank" href="D:/casestudy_modul1/img/' + this.listProducts[i].image + '" > ' + this.listProducts[i].image + ' </a>' + '</li>';
+            sout += '<li style="width:200px;">' + this.listProducts[i].total + '</li>';
             sout += '<li style="width:130px;">' + this.listProducts[i].price + '</li>';
             sout += '<li style="width:130px;">' + this.listProducts[i].status + '</li>';
-            sout += '<li><button class="button" type="button" onclick="deleteSp('+i+')">Xoa</button>';
-            sout += '<li><button class="button" type="button" onclick="editSp('+i+')">Sửa</button>';
-            sout += '<li><button class="button" type="button" onclick="saveSp('+i+')">Save</button>';
+            sout += '<li><button class="button" type="button" onclick="deleteSp(' + i + ')">Xoa</button>';
+            sout += '<li><button class="button" type="button" onclick="editSp(' + i + ')">Sửa</button>';
+            sout += '<li><button class="button" type="button" onclick="saveSp(' + i + ')">Save</button>';
             sout += '</ul>';
         }
         this.saveStore();
@@ -127,20 +132,21 @@ function show() {
 
 }
 
-function deleteSp(i){
+function deleteSp(i) {
+    
     pro.delete(i);
 }
 
-function add(){
+function add() {
     pro.addProduct();
-    
+    return window.location = "product.html";
 }
 
-function editSp(i){
+function editSp(i) {
     pro.edit(i);
 }
 
-function saveSp(i){
+function saveSp(i) {
     pro.save(i);
 }
 
